defb921e5509e27a551e26d0185949b1a49bebf2
erebuseternal <marcelsanders96@gmail.com>
2016-04-19 15:23:03 +0000

    added some drawings

--- example.py
+++ example.py
@@ -32,8 +32,8 @@ with a space of dimension 3 and greater and I haven't implemented how to draw
 that quite yet.
 """
 
-m = Matrix([[2,1],[1,2]])
+m = Matrix([[2,1],[1,1]])
 
 k = Kirchhoff(m, m.T, [1,1], 3)
-k.Find('kirchhoff')
+k.Find('drawings/kirchhoff')
 print(k.incidence_matrix)
