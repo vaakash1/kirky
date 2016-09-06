7bf3d230564de9b3c688b434c9d53422002af140
erebuseternal <marcelsanders96@gmail.com>
2016-09-06 17:05:25 -0400

    positive solve seems to be working

--- kirky/positive_solve.py
+++ kirky/positive_solve.py
@@ -1,6 +1,8 @@
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from issue import Issue
-from fractions import Fraction
+from fractions import Fraction, gcd
+import numpy as np
+from numpy.linalg import svd
 
 
 def reflect(_list):
@@ -12,16 +14,22 @@ def dot(vector1, vector2):
 
 
 class Divider(object):
+    # the svm calculator works well until one vector belongs to both classes
 
-    def __init__(self, margin=10**-4):
-        self.vectors = []
+    def __init__(self):
+        self.vectors = set()
         self.training_data = []
         self.classifications = []
-        self.svm = LinearSVC(loss='hinge')
-        self.margin = margin
+        self.svm = SVC(kernel='linear')
+        self.margin = Fraction(1,1)
 
     def add(self, vector):
-        self.vectors.append(vector)
+        size = Fraction(abs(sum(vector)))
+        if size == 0: size = 1.0
+        normed_vector = [element / size for element in vector]
+        tup = tuple(normed_vector)
+        if tup not in self.vectors:
+            self.vectors.add(tup)
 
     def _create_training_data(self):
         """
@@ -30,21 +38,34 @@ class Divider(object):
         these two groups, then we can find a hyperplane through the origin that divides the space into
         two where all of the input vectors are on one side.
         """
-        num_vectors = len(self.vectors)
         # we create the training data from our normal vectors and the reflected vectors
-        self.training_data = [vector for vector in self.vectors] + [reflect(vector) for vector in self.vectors]
+        # svm doesn't like when one vector belongs to two classes so we are avoiding that here
+        # by only taking reflections that aren't in the vectors set
+        reflections = []
+        for vector in self.vectors:
+            reflection = tuple(reflect(vector))
+            if reflection not in self.vectors:
+                reflections.append(reflection)
+        self.training_data = [vector for vector in self.vectors] + reflections
         # we create their classifications
-        self.classifications = [0] * num_vectors + [1] * num_vectors
+        self.classifications = [0] * len(self.vectors) + [1] * len(reflections)
+        # now we check to see if we have one of class 1. If we don't we know all the vectors have their reflection
+        # present, so either all of these vectors lie in the same plane (in which case we need to choose a side)
+        # or they don't (and this is going to fail regardless
+        # so we create that extra vector to pick a side
+        for arb_vector in self.vectors:
+            break
+        refl_vector = reflect(arb_vector)
+        new_vector = [(arb_vector[i] + refl_vector[i])/2.0 for i in range(len(arb_vector))]
+        self.training_data.append(new_vector)
+        self.classifications.append(1)
 
     def divide(self):
         self._create_training_data()
         self.svm.fit(self.training_data, self.classifications)
         # we get the norm to the plane that our model thinks divides the two sets
         # I know this looks weird, but the coefficients are an array in a list so its weird...
-        svm_norm = self.svm.coef_[0].tolist()
-        # this is here because we know that we shall be dealing in integers
-        norm = [Fraction(element).limit_denominator(int(1.0 / self.margin) - 1) for element in svm_norm]
-        print norm
+        norm = self.svm.coef_[0].tolist()
         # next we run through the vectors and see if their dot product with the norm is all
         # positive, if the norm is the wrong way on the first dot product that isn't zero we flip it once
         has_flipped = False
@@ -59,4 +80,48 @@ class Divider(object):
         # now we remove everything from the elements of the norm that are as small as or smaller than the margin
         # now we can return the norm
         return norm
+
+
+# this gets the null space
+def null(matrix, atol=10**-14, rtol=0):
+    matrix = np.atleast_2d(matrix)
+    u, s, vh = svd(matrix)
+    tol = max(atol, rtol * s[0])
+    nnz = (s >= tol).sum()
+    ns = vh[nnz:].conj().T
+    return ns
+
+
+# this gets rid of anything on the order of 10**-14
+def shave(matrix, tol=10**-14):
+    rows = []
+    for row in matrix:
+        new_row = [Fraction(element).limit_denominator(int(1.0/tol)) for element in row]
+        rows.append(new_row)
+    return np.array(rows)
+
+
+# this gets the positive of the matrix if it exists
+def positive(matrix):
+    trans = np.transpose(matrix)
+    vectors = [row for row in trans]
+    divider = Divider()
+    for vector in vectors:
+        divider.add(vector)
+    try:
+        norm = divider.divide()
+    except Issue:
+        raise Issue('no positive exists')
+    norm = np.array([[element for element in norm]])
+    return np.dot(norm, matrix)
+
+
+def positive_null(matrix):
+    n = np.transpose(null(matrix))
+    n = shave(n)
+    try:
+        return positive(n)
+    except Issue:
+        return None
+
 
