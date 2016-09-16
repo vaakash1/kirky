8da1c667eda49dc328e5d8c747f8476bc9421f80
Marcel Sanders <marcelsanders96@gmail.com>
2016-09-16 09:21:16 -0400

    fixed examples

--- example.py
+++ example.py
@@ -15,7 +15,7 @@ To generate the Kirchhoff Graph for the matrix
 1 0 -1 1 1
 0 1  1 2 1
 """
-matrix = np.array([[-2,-2],[2,3]])
+matrix = np.array([[-1,1,1],[1,2,1]])
 k = Kirchhoff(matrix)
 k.find('drawings/kirchhoff2')
 
@@ -24,6 +24,6 @@ To generate the Kirchhoff Graph for the matrix
 1 0 -1 1  3
 0 1  1 2 -1
 """
-matrix = np.array([[2,1, 1],[1,2, 1]])
+matrix = np.array([[-1,1, 3],[1,2, -1]])
 k = Kirchhoff(matrix)
 k.find('drawings/kirchhoff3')
