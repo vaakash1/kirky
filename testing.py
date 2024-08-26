f3edba05cc04804430ef30c416453d7a545eec7f
vaakash1 <126795529+vaakash1@users.noreply.github.com>
2024-08-26 08:52:21 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,6 +2,6 @@ from kirky import Kirchhoff
 from kirky.block_q import *
 from kirky.imagine import draw_graph, draw3d
 import numpy as np
-matrix = np.array([[1, 1, 3], [1, 2, -1]])
-k = Kirchhoff(matrix, q = 3, search_depth=2)
-k.draw_solution(np.array([1, 0]), np.array([0, 1]))
+matrix = np.array([[0, 1], [1, 0], [1, 2], [2, 1]])
+k = Kirchhoff(matrix)
+k.draw_solution(np.array([1, 0, 1.414, 0.618]), np.array([0, 1, 0.618, 1.414]))
